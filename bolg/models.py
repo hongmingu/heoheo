c6a53ffee919f8fbcf4a73c2f1b736cb041ddf1e
Keepair-PC\Keepair <gmh397@nate.com>
2016-09-01 01:11:59 +0900

    saved

--- bolg/models.py
+++ bolg/models.py
@@ -1,7 +1,5 @@
 from django.contrib.gis.db import models
 from django.utils import timezone
-from imagekit.models import ImageSpecField
-from imagekit.processors import ResizeToFill
 from django.conf import settings
 from django.contrib.auth.models import User
 
@@ -11,15 +9,8 @@ class Post(models.Model):
     # Regular Django fields corresponding to the attributes in the
     # world borders shapefile.
     author = models.ForeignKey(User, related_name='related_postwriter')
-    title = models.CharField(max_length=200, blank = False)
     text = models.TextField(blank = True)
     image = models.ImageField(null = False, blank = True, upload_to='images')
-    image_thumbnail = ImageSpecField(
-        source='image',
-        processors=[ResizeToFill(100, 50)],
-        format='JPEG',
-        options={'quality': 60}
-        )
     created_date = models.DateTimeField(
         default=timezone.now
         )
@@ -29,13 +20,3 @@ class Post(models.Model):
         return self.title
     # GeoDjango-specific: a geometry field (MultiPolygonField)
     # Returns the string representation of the model.
-
-
-class Comment(models.Model):
-    post = models.ForeignKey('bolg.Post', related_name='related_comments')
-    author = models.ForeignKey(User, related_name='related_commentwriter')
-    text = models.TextField(max_length=500)
-    created_date = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return self.text

33c900ce64dc27d2c3fffd544309abb859c525d2
Keepair-PC\Keepair <gmh397@nate.com>
2016-09-13 18:40:58 +0900

    ㅂㅂㅂㅂㅂㄷㄷㄷㄷㄷㄷ

--- New/mysite/urls.py
+++ New/mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView, RedirectView
+from django.conf.urls.static import static #이거 나중에 확인 ask django 라이브코딩쇼 40분 만들기 1시간-10분쯤
 
 
 urlpatterns = [
@@ -62,4 +63,4 @@ urlpatterns = [
     url(r'^accounts/profile/$', RedirectView.as_view(url='/', permanent=True), name='profile-redirect'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #여기도 라이브코딩쇼 보고 확인해야함

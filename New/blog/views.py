1b84671e43101933369fe55c90afd2d95aaae177
Keepair-PC\Keepair <gmh397@nate.com>
2016-09-16 13:42:01 +0900

    열이받는다

--- New/blog/views.py
+++ New/blog/views.py
@@ -24,20 +24,20 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
 
     def get_queryset(self) :
-    lon = self.request.GET.get('user_lon', '13')
-    lat = self.request.GET.get('user_lat', '15')
-    userpoint = GEOSGeometry('POINT(' + lon + ' ' + lat + ')', srid=4326)
-    self.result = []
-    i = 1
-    while i<20:
-        elasped_minutes = datetime.now() - timedelta(minutes=10*i)
-        list_i = Post.objects.filter(created_date__gte = elasped_minutes).filter(point__distance_lte = (userpoint, D(km=i)))
-        [self.result.append(v) for v in list_i if v not in self.result]
-        if len(self.result) > 10:
-            self.result = self.result[:8]
-            break
-        i += 1
-    return self.result
+        lon = self.request.GET.get('user_lon', '13')
+        lat = self.request.GET.get('user_lat', '15')
+        userpoint = GEOSGeometry('POINT(' + lon + ' ' + lat + ')', srid=4326)
+        self.result = []
+        i = 1
+        while i<20:
+            elasped_minutes = datetime.now() - timedelta(minutes=10*i)
+            list_i = Post.objects.filter(created_date__gte = elasped_minutes).filter(point__distance_lte = (userpoint, D(km=i)))
+            [self.result.append(v) for v in list_i if v not in self.result]
+            if len(self.result) > 10:
+                self.result = self.result[:8]
+                break
+            i += 1
+        return self.result
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)

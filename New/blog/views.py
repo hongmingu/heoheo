378355d9d976fd9d4fd5ba810fbf5f4e7df848b2
Keepair-PC\Keepair <gmh397@nate.com>
2016-09-26 18:10:02 +0900

    no message

--- New/blog/views.py
+++ New/blog/views.py
@@ -9,13 +9,7 @@ from blog.serializers import PostSerializer, UserSerializer
 from rest_framework import filters, viewsets, generics
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
-from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
-from rest_auth.registration.views import SocialLoginView
 from django.contrib.auth.models import User
-
-
-class FacebookLogin(SocialLoginView):
-    adapter_class = FacebookOAuth2Adapter
 
 
 class PostViewSet(viewsets.ModelViewSet):
